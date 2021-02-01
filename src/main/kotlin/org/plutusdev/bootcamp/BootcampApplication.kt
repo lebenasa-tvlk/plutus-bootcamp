a753fc81e76ce231d59430fed43945edbf2a1255
Leben Asa <leben.asa@traveloka.com>
2021-02-01 18:37:49 +0700

    No need to import test fixtures

--- src/main/kotlin/org/plutusdev/bootcamp/BootcampApplication.kt
+++ src/main/kotlin/org/plutusdev/bootcamp/BootcampApplication.kt
@@ -3,8 +3,6 @@ package org.plutusdev.bootcamp
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-import org.plutusdev.bootcamp.controller.HealthCheck
-
 @SpringBootApplication
 class BootcampApplication
 
