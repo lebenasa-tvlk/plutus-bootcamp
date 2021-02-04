f1fb8707f2960e5c61f1a1434fdb042bee0d692d
Leben Asa <leben.asa@traveloka.com>
2021-02-04 16:18:18 +0700

    Add correctly generated jooq code

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("nu.studer.jooq") version "5.2"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 }
@@ -17,9 +18,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.postgresql:postgresql:42.2.14")
+    runtimeOnly("org.postgresql:postgresql:42.2.14")
+	jooqGenerator("org.postgresql:postgresql:42.2.14")
 }
 
 tasks.withType<KotlinCompile> {
@@ -31,4 +37,43 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set(dependencyManagement.importedProperties["jooq.version"])
+	edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+	configurations {
+	    create("main") {
+	    	generateSchemaSourceOnCompilation.set(false)
+
+			jooqConfiguration.apply {
+				logging = org.jooq.meta.jaxb.Logging.WARN
+				jdbc.apply {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://localhost:5432/bootcamp_db"
+                    user = "plutus_dev"
+					password = "plutus_dev"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "bootcamp"
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+					}
+					target.apply {
+						packageName = "org.plutusdev.jooq"
+						directory = "src/main/java"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
+	}
 }
